01576a89468cfd5c7c8e22c3d6919973aa2e43bb
Giles Knap <giles.knap@diamond.ac.uk>
2020-02-13 14:18:06 +0000

    test_main

--- test/test_all.py
+++ test/test_all.py
@@ -5,4 +5,5 @@ from versiongit_test.main import main
 
 # https://github.com/bdarnell/plop/blob/master/plop/test/collector_test.py
 class ProfilerTest(unittest.TestCase):
-    assert main() == 42
+    def test_main(self):
+        assert main() == 42
